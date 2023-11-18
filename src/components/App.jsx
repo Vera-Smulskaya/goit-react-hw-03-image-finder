31d189bf89144d4fd76198c02cb42bd8e1416382
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-11-18 16:32:57 +0200

    add logic to button component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,19 @@ import ImageGallery from './ImageGallery/ImageGallery';
 import Searchbar from './Searchbar/Searchbar';
 import axios from 'axios';
 import css from './App.module.css';
+import Button from './Button/Button';
 
 const KEY = '39479425-6a3db35f3651c21ffc7f636b4';
 
 export class App extends Component {
   state = {
     images: [],
+    page: 1,
+    searchImages: '',
     isLoading: false,
+    showLoadMore: false,
+    showModal: false,
+    largeImageURL: [],
     error: null,
   };
 
@@ -23,11 +29,23 @@ export class App extends Component {
   componentDidMount() {
     this.fetchImages();
   }
+
+  onLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  showModal = () => {
+    console.log('Show modal');
+  };
+
   render() {
     return (
       <div className={css.app}>
         <Searchbar />
-        <ImageGallery />
+        <ImageGallery items={this.state.images} showModal={this.showModal} />
+        <Button onClick={this.onLoadMore}>Load more</Button>
       </div>
     );
   }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,9 +3,11 @@ import css from './Button.module.css';
 
 export default class Button extends Component {
   render() {
+    const { children, onClick } = this.props;
+
     return (
-      <button type="button" onClick={this.props.onClick} className={css.button}>
-        {this.props.children}
+      <button type="button" onClick={onClick} className={css.button}>
+        {children}
       </button>
     );
   }
